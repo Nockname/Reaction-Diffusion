39a219d6fa63cb22e938107a9f345bb8665640cf
Nockname <nicholas.d.hagedorn@gmail.com>
2021-05-22 15:25:16 -0400

    Pre Image Stretching

--- graphics.py
+++ graphics.py
@@ -11,35 +11,25 @@ from math import log
 class Graphics:
     @staticmethod
     def __convert(aLevel, bLevel, CONVERT_METHOD, COLORWHEEL):
-        if CONVERT_METHOD=="linear":
-            aMagnitudePercent = aLevel
-            bMagnitudePercent = bLevel
-        elif CONVERT_METHOD=="expontial":
-            aMagnitudePercent=10**(aLevel-1)
-            bMagnitudePercent=10**(bLevel-1)
 
-        magnitudePercent=(aMagnitudePercent+bMagnitudePercent)/2
+        Red=int((COLORWHEEL[4]-COLORWHEEL[0])*aLevel+COLORWHEEL[0])
+        Green=int((COLORWHEEL[5]-COLORWHEEL[1])*(aLevel+bLevel)/2+COLORWHEEL[1])
+        Blue=int((COLORWHEEL[6]-COLORWHEEL[2])*bLevel+COLORWHEEL[2])
 
-        try:
-            Red=int((COLORWHEEL[4]-COLORWHEEL[0])*aMagnitudePercent+COLORWHEEL[0])
-            Green=int((COLORWHEEL[5]-COLORWHEEL[1])*magnitudePercent+COLORWHEEL[1])
-            Blue=int((COLORWHEEL[6]-COLORWHEEL[2])*bMagnitudePercent+COLORWHEEL[2])
-            Alpha=int((COLORWHEEL[7]-COLORWHEEL[3])*magnitudePercent+COLORWHEEL[3])
-        except:
-            print(aMagnitudePercent)
-
-        return [Red, Green, Blue, Alpha]
+        return [Red, Green, Blue, 255]
 
     @staticmethod
-    def __image(aStrength, bStrength, frame):
+    def __image(aStrength, bStrength, previousPixels, frame):
 
+        print("Doing math")
         aStrength, bStrength=Calculate.updateAllStrength(aStrength, bStrength)
 
-        pixels=np.ndarray( (HEIGHT, WIDTH, 4) )
+        pixels=previousPixels
 
         aSum=0
         bSum=0
 
+        print("Doing graphics")
         for x in range(WIDTH):
             for y in range(HEIGHT):
                 pixels[HEIGHT-y-1][x][0:4]=Graphics.__convert(aStrength[x][y], bStrength[x][y],
@@ -47,17 +37,19 @@ class Graphics:
                 aSum+=aStrength[x][y]
                 bSum+=bStrength[x][y]
 
-
+        print("Saving Image")
         print("{} amount of A, {} amount of B, frame {}, {} percent done".format(aSum, bSum, frame, frame/FRAMES))
         image=Image.fromarray(np.uint8(pixels)).convert('RGB')
         image.save("./data/{}.png".format(frame))
 
-        return aStrength, bStrength
+        return aStrength, bStrength, pixels
 
     @staticmethod
     def createVideo(aStrength, bStrength): 
+
+        pixels=np.ndarray( (HEIGHT, WIDTH, 4) )
         for frame in range(0, FRAMES):
-            aStrength, bStrength=Graphics.__image(aStrength, bStrength, frame)
+            aStrength, bStrength, pixels=Graphics.__image(aStrength, bStrength, pixels, frame)
 
         if CREATE_VIDEO:
             os.system("ffmpeg -framerate {} -start_number 1 -i ./data/%d.png -vcodec libx264 -pix_fmt yuv420p {}.mov".format(FPS, VIDEO_NAME))
